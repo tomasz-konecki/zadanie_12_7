86dd17a5fa968f92c89813b4cf5837f399889ef5
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-30 11:32:31 +0200

    Finish excercise

--- Board.js
+++ Board.js
@@ -8,9 +8,20 @@ var board = {
 };
 
 $('.create-column')
-    .click(function(){
-        board.createColumn(new Column(prompt('Wpisz nazwę kolumny')));
-    });
+    .click(function() {
+        var columnName = prompt('Enter a column name');
+        $.ajax({
+            url: baseUrl + '/column',
+            method: 'POST',
+            data: {
+                name: columnName
+            },
+            success: function(response){
+                var column = new Column(response.id, columnName);
+                board.createColumn(column);
+            }
+        });
+});
     
 function initSortable() {
     $('.card-list').sortable({
